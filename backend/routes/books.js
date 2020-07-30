473cffe2970310323af8542f667e2e221ae7ef9a
felipe-gerula <felipe.gerula@gmail.com>
2020-07-29 23:38:32 -0300

    rest api con Nodejs y MongoDB

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,9 +1,24 @@
 const {Router} = require("express");
 const router = Router();
 
-router.get("/" , (req , res) =>{
-  res.json({text : "Hello World"});
+ const Book = require("../models/Book");
+
+router.get("/" , async (req , res) =>{
+  const books = await Book.find();
+  res.json(books);
 });
 
+router.post("/",async(req , res)=>{
+   const {title , author , isbn} = req.body;
+   const newBook = new Book({title,author,isbn});
+   await newBook.save();
+   res.json({message : "book saved"});
+});
+
+
+router.delete("/:id", async(req,res) => {
+  await Book.findByIdAndDelete(req.params.id);
+  res.json({message : "book deleted"});
+});
 
 module.exports = router;
