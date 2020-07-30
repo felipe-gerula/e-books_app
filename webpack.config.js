b99386ebbd999015b6cb69501a289e4fddc793ee
felipe-gerula <felipe.gerula@gmail.com>
2020-07-30 00:40:09 -0300

    minificar codigo HTML

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const htmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+ entry :"./frontend/app.js",
+ output:{
+     path : path.join(__dirname,"backend/public"),
+     filename : "bundle.js" 
+ },
+
+ plugins : [
+     new htmlWebPackPlugin({
+      template : "./frontend/index.html",
+      minify : {
+          collapseWhitespace : true,
+          removeComments : true,
+          removeRedundantAttributes : true,
+          removeScriptTypeAttributes : true,
+          removeStyleLinkTypeAttributes : true,
+          useShortDoctype : true
+      } 
+     })
+    ] 
+
+}
